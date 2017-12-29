acdbfb87e1fcdce7b10ff741b55ff491c06eb789
zhaopan7011 <zhaopan7011@163.com>
2017-12-29 16:34:56 +0800

    创建项目

---
+++ MyLibRespository/MyLibRespository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLibRespository
+//
+//  Created by 易水寒 on 2017/12/29.
+//  Copyright © 2017年 Deyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLibRespository/MyLibRespository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLibRespository
+//
+//  Created by 易水寒 on 2017/12/29.
+//  Copyright © 2017年 Deyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
